c2264442b7ad704544e10382e72a26e56ac51bcb
unknown <draszerpl@wp.pl>
2025-02-19 16:35:24 +0100

    Save predictions along with input data and timestamp after each prediction request

--- app/models/database_models.py
+++ app/models/database_models.py
@@ -1,27 +1,27 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, DECIMAL
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+from datetime import datetime, timezone
 
 Base = declarative_base()
 
 class Prediction(Base):
     __tablename__ = 'price_predictions'
     
-    prediction_id = Column(Integer, primary_key=True, index=True)
-    brand = Column(String)
-    production_year = Column(Integer)
-    used_or_new = Column(String)
-    transmission = Column(String)
-    drive_type = Column(String)
-    fuel_type = Column(String)
-    fuel_consumption = Column(String)
-    kilometres = Column(Integer)
-    cylinder_in_engine = Column(Integer)
-    body_type = Column(String)
-    doors = Column(Integer)
-    seats = Column(Integer)
-    prediction_price = Column(Integer)
-    timestamp = Column(DateTime, default=datetime.utcnow)
+    prediction_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    brand = Column(String, nullable=False)
+    production_year = Column(Integer, nullable=False)
+    used_or_new = Column(String, nullable=False)
+    transmission = Column(String, nullable=False)
+    drive_type = Column(String, nullable=False)
+    fuel_type = Column(String, nullable=False)
+    fuel_consumption = Column(String, nullable=False)
+    kilometres = Column(Integer, nullable=False)
+    cylinder_in_engine = Column(Integer, nullable=False)
+    body_type = Column(String, nullable=False)
+    doors = Column(Integer, nullable=False)
+    seats = Column(Integer, nullable=False)
+    prediction_price = Column(DECIMAL, nullable=False)
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
         
     def __repr__(self):
         return (f"Prediction(ID: {self.prediction_id}, "
--- app/routes/prediction_routes.py
+++ app/routes/prediction_routes.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.models.prediction_models import InputData
 from app.services.model_handler import ModelHandler
 from app.core.config import Config
+from app.database.dependencis import get_db
+from app.models.database_models import Prediction
+from datetime import datetime, timezone
+from sqlalchemy.orm import Session
 
 # FastAPI application setup
 router = APIRouter()
@@ -20,6 +24,25 @@ def healthcheck():
 
 # Prediction endpoint
 @router.post('/predict')
-def predict(input_data: InputData):
+def predict(input_data: InputData, db: Session = Depends(get_db)):
     prediction = model_handler.predict(input_data.model_dump())
+    db_prediction = Prediction(
+        brand=input_data.Brand,
+        production_year=input_data.Year,
+        used_or_new=input_data.UsedOrNew,
+        transmission=input_data.Transmission,
+        drive_type=input_data.DriveType,
+        fuel_type=input_data.FuelType,
+        fuel_consumption=input_data.FuelConsumption,
+        kilometres=input_data.Kilometres,
+        cylinder_in_engine=input_data.CylindersinEngine,
+        body_type=input_data.BodyType,
+        doors=input_data.Doors,
+        seats=input_data.Seats,
+        prediction_price=round(prediction[0], 2),
+        timestamp=datetime.now(timezone.utc) 
+    )
+    db.add(db_prediction)
+    db.commit()
+    db.refresh(db_prediction)
     return {"prediction": prediction}
